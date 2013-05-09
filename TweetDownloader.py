2584b79a823c462438b7a6db5edcd7ddb8022879
Daniel Mané <danmane@gmail.com>
2013-05-09 01:47:21 -0500

    Documentation strings and arguments

--- TweetDownloader.py
+++ TweetDownloader.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python
 import twitter, sys, re, datetime, time, dateutil.parser, pytz
 CST = pytz.timezone("US/Central")
-TIMER = True
 
 class TweetDownloader:
-	def __init__(me, search_string, search_mode = True, verbose = False):
-		# SearchMode: True -> Search (phrase can be anywhere in 
-		# text) False -> Match (phrase must be at start of text)
+	"""Downloads Tweets matching a given search string and regular expression pattern.
+	TODO: Implement public interface get_Tweets(freshness) which returns all matching Tweets
+	which are fewer than (freshness) seconds old.
+	Takes a search string and options dictionary with parameters:
+	"REQUIRE_MATCH": True  -> find regex only at start of tweet 	
+					 False -> find regex anywhere in tweet
+	"BLOCK_RETWEETS": True -> Block recent retweets (NOT IMPLEMENTED)
+	"VERBOSE"    :  True -> Print a lot of stuff
+	"TIMER"      :  True -> Print download times
+	"POSTFIX" : a regular expression string to append after the search string
+	"PREFIX"  : a regular expression string to prepend to the search string
+	"CASE_SENSITIVE": True -> Enable case sensitivity"""
+
+	def __init__(me, search_string, require_match=False, block_retweets=False, 
+		verbose=False, timer=False, prefix="", postfix=r"[^\.\?!\n:,#]*[\.\?!]*",
+		case_sensitive=False):
+		me.require_match  = require_match
+		me.block_retweets = block_retweets #Not implemented
+		me.verbose        = verbose
+		me.timer          = timer
+		
 		me.api = twitter.Api()
 		me.search_string = search_string
 		me.quoted_string = "\"" + search_string + "\""
 		me.max_id = None
-		me.pattern = search_string + r"[^\.\?!\n:,#]*[\.\?!]*"
-		me.re = re.compile(me.pattern, flags=re.IGNORECASE)
-		me.search_mode = search_mode
-		me.verbose = verbose
+		me.pattern = prefix + search_string + postfix
+		flags = None if case_sensitive else re.IGNORECASE
+		me.re = re.compile(me.pattern, flags=flags)
 
-	def process_tweet(me, tweet):
-		# Uses regular expressions to simplify the tweet to contain
-		# a sentance or phrase starting with the search expression
-		# eg: "Argh Dan, why are you so awesome?!" with search 
-		# string "why are you" goes to 
-		# "why are you so awesome?!"
-		# Depends on tweet having a .text and .search_string attribute
-		# creates a .simpletext attribute
-		# Returns True if processing is successful
-		# Returns False if unable to process (e.g. RE didnt match)
+	def process_Tweet(me, tweet):
+		"""Uses regular expressions to simplify the tweet to contain
+		 a sentance or phrase starting with the search expression
+		 eg: "Argh Dan, why are you so awesome?!" with search 
+		 string "why are you" goes to 
+		 "why are you so awesome?!"
+		 Depends on tweet having a .text and .search_string attribute
+		 creates a .simpletext attribute
+		 Returns True if processing is successful
+		 Returns False if unable to process (e.g. RE didnt match)
+		 If processing is successful, it adds attributes 
+		 Tweet.phrase (the matching expression) and Tweet.time
+		 (a DateTime object)"""
 		tweet.text = tweet.text.encode('utf-8', 'ignore')
-		if me.search_mode: 
-			match = me.re.search(tweet.text)
-		else:
+		if me.require_match: 
 			match = me.re.match(tweet.text)
+		else:
+			match = me.re.search(tweet.text)
+
 		if not match:
 			if me.verbose:
+				print "Failed to match: "
 				print "TWEET: ", tweet.text
-				print "PATTERN: " + me.pattern + " NO MATCH"
+				print "PATTERN: ", me.pattern
 			return False
 
 		phrase = match.group()
@@ -46,6 +67,8 @@ class TweetDownloader:
 		return True
 
 	def search(me):
+		"""Private function which searches Twitter for new Tweets with the target phrase, and processes them to see if they 
+		have a phrase matching our regular expression."""
 		if TIMER: search_start = time.time()
 		results = me.api.GetSearch(me.quoted_string, since_id=me.max_id)
 		if TIMER: 
@@ -57,7 +80,7 @@ class TweetDownloader:
 
 		me.max_id = max(r.id for r in results) # i <3 generators
 
-		goodresults = [r for r in results if me.process_tweet(r)]
+		goodresults = [r for r in results if me.process_Tweet(r)]
 		
 		if me.verbose:
 			print "=========================", len(results)
